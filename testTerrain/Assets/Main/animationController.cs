f8fa00cb22dbc4cbb9769fb3af1ba4ff53073410
HOSHINOANNA <49102292+HOSHINOANNA@users.noreply.github.com>
2019-03-31 15:09:00 +0000

    First Commit

---
+++ testTerrain/Assets/Main/animationController.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class animationController : MonoBehaviour
+{
+    public Animator animator;
+
+
+
+    private Transform tr;
+    private Vector3 localVelocity;
+
+    private Vector3 lastPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = this.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // MOVEMENT KEYBOARD CONTROLLER
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            animator.SetBool("Walk", true);
+
+            gameController();
+
+        }
+        if (Input.GetKeyUp(KeyCode.W))
+        {
+            animator.SetBool("Walk", false);
+
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            animator.SetBool("Walk", true);
+
+        }
+
+
+        if (Input.GetKeyUp(KeyCode.S))
+        {
+            animator.SetBool("Walk", false);
+
+        }
+
+        gameController();
+        // ATTACK WING KEYBOARD CONTROLLER
+
+    }
+
+    void gameController()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            animator.SetBool("Atk1_wing", true);
+        }
+        if (Input.GetKeyUp(KeyCode.Q))
+        {
+            animator.SetBool("Atk1_wing", false);
+        }
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            animator.SetBool("Atk2_wing", true);
+        }
+        if (Input.GetKeyUp(KeyCode.E))
+        {
+            animator.SetBool("Atk2_wing", false);
+        }
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            animator.SetBool("Atk3_wing", true);
+        }
+        if (Input.GetKeyUp(KeyCode.R))
+        {
+            animator.SetBool("Atk3_wing", false);
+        }
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            animator.SetBool("Run", true);
+        }
+        if (Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            animator.SetBool("Run", false);
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            animator.SetBool("Jump", true);
+        }
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            animator.SetBool("Jump", false);
+        }
+
+    }
+}
---
+++ testTerrain/Assets/Main/keyboard.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class keyboard : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private int State;
+    private int oldState = 0;
+    private int UP = 0;
+    private int RIGHT = 1;
+    private int DOWN = 2;
+    private int LEFT = 3;
+    private int RUN = 4;
+
+
+    public float speed = 2.5f;
+
+    void Start()
+    {
+    }
+    void Update()
+    {
+        
+        if (Input.GetKey(KeyCode.W))
+        {
+            setState(UP);
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            setState(DOWN);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            setState(LEFT);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            setState(RIGHT);
+        }
+
+
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            speed = 4f;
+            
+        }
+
+        if (Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            speed = 2.5f;
+
+        }
+    }
+
+
+    void setState(int currState)
+    {
+        Vector3 transformValue = new Vector3();
+        int rotateValue = (currState - State) * 90;
+
+        switch (currState)
+        {
+            case 0:
+                transformValue = Vector3.forward * Time.deltaTime * speed;
+              
+               break;
+            case 1:
+                transformValue = Vector3.right * Time.deltaTime * speed;
+               
+                break;
+            case 2:
+                transformValue = Vector3.back * Time.deltaTime * speed;
+            
+                break;
+            case 3:
+                transformValue = Vector3.left * Time.deltaTime * speed;
+              
+                break;
+        }
+
+        transform.Rotate(Vector3.up, rotateValue);
+        transform.Translate(transformValue, Space.World);
+        oldState = State;
+        State = currState;
+
+    }
+}
