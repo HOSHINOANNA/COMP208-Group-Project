d8480b52ea03e3b89aac082108263018ffb07943
chris0327 <49123361+chris0327@users.noreply.github.com>
2019-04-03 00:58:44 +0100

    start

---
+++ ui/Assets/MainMenu.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement; //场景管理器
+public class MainMenu : MonoBehaviour
+{
+    public void playGame()
+    {
+        SceneManager.LoadScene("gameTest");//代表场景名字
+        //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+1);   也可以用索引
+    }
+    public void QuitGame()
+    {
+        Debug.Log("QUIT!");
+        Application.Quit();
+    }
+
+}
---
+++ ui/Assets/controlVolume.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class controlVolume : MonoBehaviour
+{
+    public AudioSource asound;
+    public Slider sd;
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void Con_volume()
+    {
+        asound.volume = sd.value;
+    }
+}
